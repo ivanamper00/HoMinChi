fdd44dae0592f7b7966bd79bc1994e1a7337bcaf
Ivan Amper <ivanamper00@github.com>
2022-06-22 16:22:50 +0800

    update version

--- app/src/main/java/sorr/ynasir/hominchi/dashboard/adapter/CarouselAdapter.kt
+++ app/src/main/java/sorr/ynasir/hominchi/dashboard/adapter/CarouselAdapter.kt
@@ -6,10 +6,17 @@ import androidx.recyclerview.widget.RecyclerView
 import sorr.ynasir.hominchi.databinding.CarouselviewBinding
 
 class CarouselAdapter(val dataImg : ArrayList<Int>): RecyclerView.Adapter<CarouselAdapter.AdapterHolder>() {
+
     class AdapterHolder (val adpts : CarouselviewBinding) : RecyclerView.ViewHolder(adpts.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterHolder = AdapterHolder(
-        CarouselviewBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterHolder =
+        AdapterHolder(
+            CarouselviewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
     override fun onBindViewHolder(holder: AdapterHolder, position: Int) {
         holder.adpts.apply {
